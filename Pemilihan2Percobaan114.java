231a16acbf111d4ee5116ee62171c4b0cdbfad70
kmilaz14 <kamilazhwa@gmail.com>
2024-10-03 09:33:16 +0800

    pertanyaan 4

--- Pemilihan2Percobaan114.java
+++ Pemilihan2Percobaan114.java
@@ -9,12 +9,15 @@ public class Pemilihan2Percobaan114 {
         int tahun = input14.nextInt();
     
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 ) {
-                System.out.println("Tahun Kabisat");
+            if ((tahun % 100) == 0 ) {
+               if ((tahun % 400 == 0)) {
+                    System.out.println("Tahun Kabisat");
+                 } else {
+                     System.out.println("Bukan Tahun Kabisat");
+                 }
             } else {
-                System.out.println("Bukan Tahun Kabisat");
-            }
-                
+                System.out.println("Tahun Kabisat");
+            }   
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
